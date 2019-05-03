c4b587fa317012911584be735f82159d627e66da
DaaDaniela <danieladelcarmen168@hotmail.com>
2019-05-03 17:44:49 -0500

    Estructura dinamica HTML de la primera interfaz

--- src/main.js
+++ src/main.js
@@ -2,63 +2,29 @@
 
 // import { myFunction } from './lib/index.js';
 
-const registrar = () => {
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('contraseña').value;
-
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-    .catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // ...
-    });
-  }
-  registrar();
-
-const ingreso = () => {
-    const email2 = document.getElementById('email2').value;
-    const password2 = document.getElementById('contraseña2').value;
-
-    firebase.auth().signInWithEmailAndPassword(email2, password2)
-    .catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.log(errorCode);
-      console.log(errorMessage);
-      // ...
-    }); 
-  }
-ingreso();
- // Configura un observador de estado de autenticación y obtén datos del usuario  
-const observador = () => {
-    // Si existe algun cambio de usuario o alguien se registro:
-    firebase.auth().onAuthStateChanged(function(user) {
-        // Va ejecutar este, que si existe el usuario:
-        if (user) {
-            console.log('existe usuario activo');
-          // User is signed in.
-          var displayName = user.displayName;
-          var email = user.email;
-          var emailVerified = user.emailVerified;
-          var photoURL = user.photoURL;
-          var isAnonymous = user.isAnonymous;
-          var uid = user.uid;
-          var providerData = user.providerData;
-          // Sino existe Ejecuta esto:
-        } else {
-          // User is signed out.
-          console.log('No existe el usuario');
-          // ...
-        }
-      });
-      
-}
-observador();
- 
-const aparece = () => {
-    const contenido = document.getElementById('contenido');
-    contenido.innerHTML = "Solo lo ve usuario activo";
-}
 // myFunction();
+
+const ElementoPadre = document.getElementById('padre'); 
+
+const  page1 =  (nodoPadre)=>{
+const createNodoHijoDiv = document.createElement("div");
+const contenido = `<div class = "col-xs-12 col-lg-6"><img src="http://purelytwins.com/wp-content/uploads/2013/03/purely-fit-life-logos-600px3.png"/></div>
+<div class = "col-xs-12 col-lg-6"><img src="http://www.fitlifelucy.com/wp-content/uploads/2016/10/fitlifelucy.png"/>
+<h1>Bienvenida!</h1>
+<input type="text" value="Email"/> </br>
+<input type="text" value="password"/> </br>
+<input type="button" value="Log in"/>
+<h1>Puedes ingresar con ...</h1>
+<img src="https://cdn.icon-icons.com/icons2/726/PNG/128/instagram_f_icon-icons.com_62685.png" alt=""/> 
+<img src="https://cdn.icon-icons.com/icons2/413/PNG/128/Facebook_41212.png" alt=""/>
+<h1>No tienes un cuenta? REGISTRATE!</h1></div>
+`;
+createNodoHijoDiv.innerHTML = contenido;
+nodoPadre.appendChild(createNodoHijoDiv);
+}
+
+page1(ElementoPadre);
+
+
+
+
