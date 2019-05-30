a2af6e8d38c6f6c8bcf422c27727a426569bfbf5
SusanaCahuantico <scahuantico@gmail.com>
2019-05-30 14:22:12 -0500

    arreglando conflictos

--- src/view-controller/promises.js
+++ src/view-controller/promises.js
@@ -44,21 +44,14 @@ NewUsers(email, password)
   export const google = () => {
     LogGoogle()
     .then(function(result) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
     var token = result.credential.accessToken;
-    // The signed-in user info.
     var user = result.user;
-    // ..
     })
     .catch(function(error) {
-    // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
-    // The email of the user's account used.
     var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
+   var credential = error.credential;
     })
     };
     
@@ -75,5 +68,4 @@ NewUsers(email, password)
     var credential = error.credential;
     })
     };
- 
-
+    
