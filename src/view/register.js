164022b1f8134a24da6f65c87d40ae1c3bdf3de5
SusanaCahuantico <scahuantico@gmail.com>
2019-06-21 10:17:40 -0500

    css

---
+++ src/view/register.js
@@ -0,0 +1,26 @@
+import { register } from "../view-controller/promises.js";
+
+export const registerUSer = () => {
+    const CreateChildNode = document.createElement("div");
+    CreateChildNode.innerHTML = '';
+    const Content = 
+    `<div class = "col-xs-12 col-lg-6"><img src="../images/logo.png" alt=""/></div>
+    <div class = "col-xs-12 col-lg-6"><img src="../images/titulo.PNG" alt=""/>
+    <div class ="contenedor">
+    <h2>¡Empecemos con tu registro!</h2>
+    <input class ="input" id="name" type="text" placeholder="Nombre"/> </br>
+    <input class ="input" id="emailRegister" type="text" placeholder="Email"/> </br>
+    <input class ="input" id="passwordRegister" type="password" placeholder="Password"/> </br>
+    <a href="#/perfil"><button class ="botonInicio" id="btnRegister">¡Registrate!</button></a> 
+    </div>
+    </div>`
+    ;
+    CreateChildNode.innerHTML = Content;
+
+    const btnRegister = CreateChildNode.querySelector('#btnRegister')
+    btnRegister.addEventListener('click', () => {
+        register();   
+    });
+
+    return CreateChildNode;
+}
