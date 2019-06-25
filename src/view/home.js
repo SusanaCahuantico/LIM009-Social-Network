efe7e0701161926065ff6d40ba204da12b44614a
Daniela Gutierrez <danieladelcarmen168@hotmail.com>
2019-06-25 18:10:19 -0500

    Mejorando Home y Home2 con flexbox

--- src/view/home.js
+++ src/view/home.js
@@ -3,22 +3,19 @@ import { logear, google } from "../view-controller/promises.js";
 export default ()=>{
     const CreateChildNode = document.createElement("div");
     const Content = 
-    `<div class ="boxBig">
-    <div class = "col-xs-12 col-lg-6 paddImg"><img src="../images/logo.png" alt=""/></div>
-    <div>
-    <div class = "col-xs-12 col-lg-6 boxBig flexText"><img src="../images/titulo.PNG" alt=""/>
-    <div class ="contenedor">
+    `<div class = "general">
+    <div class = "logo">
+    <img src="../images/logo.png" alt=""/>
+    </div>
+    <div class = "intro">
+    <img src="../images/titulo.PNG" alt=""/>
     <h2> Bienvenida! </h2>
     <input class ="input" id="emailLogin" type="text" placeholder="Email"/> </br>
     <input class="input" id="contraseñaLogin" type="password" placeholder="Password"/> </br>
     <button class ="botonInicio" id="btnLogin">Log in</button>
     <h3>Tambien puedes ingresar con ...</h3>
-    <div class="col-xs-12 col-lg-12 spaceIcon">
-     <img class="col-xs-6 col-lg-6" id="btnGoogle" class ="iconos" src="../images/busqueda(1).png" alt=""/>
-    </div>
-    <h3>No tienes un cuenta? <a href="#/registrate"> Registrate!</a> </h3> 
-    </div>
-    </div>
+     <img id="btnGoogle" class ="iconos" src="../images/busqueda(1).png" alt=""/>
+    <h3>No tienes un cuenta? <a href="#/registrate"> Registrate!</a> </h3>
     </div>
     </div>`
     ;
@@ -29,9 +26,6 @@ export default ()=>{
 
     const botonGoogle = CreateChildNode.querySelector('#btnGoogle')
     botonGoogle.addEventListener('click', google)
-
-    //const botonFacebook = CreateChildNode.querySelector('#btnFacebook')
-    //botonFacebook.addEventListener('click', facebook)
 
 return CreateChildNode
 }
--- src/view/home2.js
+++ src/view/home2.js
@@ -2,15 +2,18 @@ import { register } from "../view-controller/promises.js";
 
 export default ()=>{
     const CreateChildNode = document.createElement("div");
-    const Content = `<div class = "col-xs-12 col-lg-6"><img src="../images/logo.png" alt=""/></div>
-    <div class = "col-xs-12 col-lg-6"><img src="../images/titulo.PNG" alt=""/>
-    <div class ="contenedor">
+    const Content = `
+    <div class ="general">
+    <div class ="logo">
+    <img src="../images/logo.png" alt=""/>
+    </div>
+    <div class = "intro">
     <h2>¡Empecemos con tu registro!</h2>
     <input class ="input" id="name" type="text" placeholder="Nombre"/> </br>
     <input class ="input" id="lastName" type="text" placeholder="Apellido"/> </br>
     <input class ="input" id="emailRegister" type="text" placeholder="Email"/> </br>
     <input class ="input" id="passwordRegister" type="password" placeholder="Password"/> </br>
-    <a href="#/home"><button class ="botonInicio" id="btnRegister">¡Registrate!</button></a> 
+    <a href="#/home"><button class ="botonInicio" id="btnRegister">¡Registrate!</button></a>
     </div>
     </div>`
     ;
