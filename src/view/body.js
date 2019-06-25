a91e8578767563af28fa09ac415dce7385a4685b
SusanaCahuantico <scahuantico@gmail.com>
2019-06-25 18:41:43 -0500

    css

--- src/view/body.js
+++ src/view/body.js
@@ -12,29 +12,25 @@ export default (posts) => {
   const Content =
   `
   <body>
-   <div class = " df col-lg-12 col-xs-12">
-      <div class="col-lg-6 col-xs-12 caja boxBig">
-        <div class="bordes col-lg-12">
-          <div class="col-lg-12 col-xs-12">
-           <img class ="portada" src ="https://images.pexels.com/photos/46024/pexels-photo-46024.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"></div>
-           <div class="col-lg-6 col-xs-6" id="painterPhoto"></div>
-           <div class="col-lg-6 col-xs-6" id="painter" class="painter-user"></div>
-          </div>
-        </div>
-      </div>
+   <div class="i">
+           <img class ="portada" src ="https://images.pexels.com/photos/46024/pexels-photo-46024.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"/>
+           <div class="perfil">
+            <p id="painterPhoto"></p>
+            <p id="painter" class="painter-user"></p>
+           </div>
     </div>
-    <div class="col-lg-6 col-xs-12 caja"> 
+    <div class="caja"> 
        <select id="estado">
        <option value="privado">Privado</option>
        <option value="publico">Público</option>
        </select>
-      <div class="box"
+       <div class="box"
        <form class="formulario" action="">
        <textarea class= "share-post line-black" type="text" id="tareaInput" placeholder="¿que estas pensando?"></textarea>
        <input type="button" id="btn-agregar" class="boton" value="Agregar Tarea">
        </form>
-      </div>
-     <div id="wrap" class="wraper"></div>
+       </div>
+      <div id="wrap" class="wraper"></div>
     </div>
   </body>
  `;
